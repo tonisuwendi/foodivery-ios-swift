337fcebbf922b347a55e02a94fee7e422d4007f2
Toni Suwendi <toni.suwendi@majoo.id>
2022-04-17 02:53:18 +0700

    Initial Commit

---
+++ Foodivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodivery
+//
+//  Created by Toni Suwendi on 17/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
